e19e1d76ddc8d8400160ab07f24d73ee9f3ad7d6
ahmedelbanna <ahmedelbanna@weaccept.co>
2018-03-26 14:42:01 +0200

    Add DELETE /users/me/token api

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -63,6 +63,14 @@ UserSchema.methods.generateAuthToken = function(){
 	});
 }
 
+UserSchema.methods.removeToken = function(token){
+	let user = this;
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+}
 UserSchema.statics.findByToken = function(token){
 	let User = this;
 	let decoded;
